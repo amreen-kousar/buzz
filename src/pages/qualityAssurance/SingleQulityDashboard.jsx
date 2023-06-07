c7404bc553982c434a1f6192cdecc93a338d66eb
ShrayesBenak <128351544+ShrayesBenak@users.noreply.github.com>
2023-06-07 14:45:44 +0530

    quality assessment

--- src/pages/qualityAssurance/SingleQulityDashboard.jsx
+++ src/pages/qualityAssurance/SingleQulityDashboard.jsx
@@ -211,8 +211,20 @@ useEffect(()=>{
                                             <Grid id="grid-own-open-filter"  item xs={8}>
                                                 <b cursor="pointer" style={{ color: "blue" }} >{itm?.name_of_the_assessor}</b><br>
                                                 </br>
-                                                {/* <Typography id="typography-ta-amount" variant="body" gutterBottom > <b>TA Amount:{itm?.telephone}</b></Typography>
-                                            */}
+                                                <Typography style={{fontSize:"0.7rem"}} > <b>{itm?.program_assessment == 1? 
+                                                <>
+                                                Trainer : {itm?.name_of_the_trainer_being_evaluated } 
+                                                </> :
+                                                itm?.program_assessment == 2? 
+                                                <>
+                                                Gelathi Facilitator : {itm?.name_of_the_gf}
+                                                </>
+                                                : itm?.program_assessment == 2? 
+                                                <>
+                                                   Gelathi : {itm?.name_of_the_gelathi_being_evaluated}
+                                                </>
+                                              : null
+                                              }</b></Typography>
                                             </Grid>
                                             <Grid item xs={4}>
                                                 {/* <Iconify id="uiicons-cross" onClick={() => { handleDeleteTA(itm) }} style={{ float: "right", marginTop: 5, marginRight: 10, fontSize: 30, color: "gray" }} icon="system-uicons:cross"></Iconify>
