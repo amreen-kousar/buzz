689e7d06920de1420805b95e388d8ea628400541
harshapriyakondamuru <harshapriya054@gmail.com>
2023-07-21 10:09:12 +0530

    qafilters for admin

--- src/pages/qualityAssurance/QualityAssuranceFilters/QualityAssurancefilter.jsx
+++ src/pages/qualityAssurance/QualityAssuranceFilters/QualityAssurancefilter.jsx
@@ -22,30 +22,7 @@ import {
   CardContent,
 } from '@mui/material';
 import AdminQuality from './Admin';
-export const SORT_BY_OPTIONS = [
-  { value: 'featured', label: 'Featured' },
-  { value: 'newest', label: 'Newest' },
-  { value: 'priceDesc', label: 'Price: High-Low' },
-  { value: 'priceAsc', label: 'Price: Low-High' },
-];
-export const FILTER_GENDER_OPTIONS = ['Men', 'Women', 'Kids'];
-export const FILTER_CATEGORY_OPTIONS = ['All', 'Shose', 'Apparel', 'Accessories'];
-export const FILTER_RATING_OPTIONS = ['up4Star', 'up3Star', 'up2Star', 'up1Star'];
-export const FILTER_PRICE_OPTIONS = [
-  { value: 'below', label: 'Below $25' },
-  { value: 'between', label: 'Between $25 - $75' },
-  { value: 'above', label: 'Above $75' },
-];
-export const FILTER_COLOR_OPTIONS = [
-  '#00AB55',
-  '#000000',
-  '#FFFFFF',
-  '#FFC0CB',
-  '#FF4842',
-  '#1890FF',
-  '#94D82D',
-  '#FFC107',
-];
+
 QualityAssuranceFilter.propTypes = {
   isOpenFilter: PropTypes.bool,
   onOpenFilter: PropTypes.func,
