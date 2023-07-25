9913edfb9ba31c0c63b663a9fb8c1e678aaf0993
aswin lohi <107166391+Aswinlohii@users.noreply.github.com>
2023-07-25 17:20:36 +0530

    validation

--- src/pages/projects/Components/GelathiCircleForm.jsx
+++ src/pages/projects/Components/GelathiCircleForm.jsx
@@ -281,7 +281,8 @@ const saveDataLocally = (key, data) => {
       listenpara != '' &&
       skillValue != '' &&
       selectedValue != ''
-    ) {
+    )
+     {
       if(localStorage.getItem('spoorthi')){
         // data  = setSpoorthiForm()
         // setSpoorthiForm(data)
@@ -339,12 +340,54 @@ const saveDataLocally = (key, data) => {
     } else {
       alert('Please Select The Option. ');
     }
-  } else{
-    // setSpoorthiForm()
-    // saveDataLocally('spoorthi', data)
-    setSpoorthiForm(saveDataLocally('spoorthi', JSON.parse(data)));
-    componentreloadmethod();
-    handleClose();
+  }
+   else{
+    if (selectedValue == '') {
+      setSessionValueError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (skillValue == '') {
+      setSkillError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (listenpara == '') {
+      setListenParaError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (community == '') {
+      setcommunityError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (communitymem == '') {
+      setcommunitymemError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (bringtogether == '') {
+      setbringtogetherError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (conflicts == '') {
+      setconflictsError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (
+      conflicts != '' &&
+      bringtogether != '' &&
+      communitymem != ' ' &&
+      community != '' &&
+      listenpara != '' &&
+      skillValue != '' &&
+      selectedValue != ''
+    ){
+      // setSpoorthiForm()
+      // saveDataLocally('spoorthi', data)
+      setSpoorthiForm(saveDataLocally('spoorthi', JSON.parse(data)));
+      componentreloadmethod();
+      handleClose();
+    }else{
+      alert("Please Select The Option")
+
+    }
   }
 
   };
