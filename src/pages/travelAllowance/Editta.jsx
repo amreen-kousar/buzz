6233206c68b9adf73b1be1debf317956ca3bad7e
Harshapriyaroyal <harshapriya054@gmail.com>
2023-08-22 16:35:33 +0530

    ta

--- src/pages/travelAllowance/Editta.jsx
+++ src/pages/travelAllowance/Editta.jsx
@@ -408,7 +408,7 @@ getTadata()
                 <TextField disabled id="outlined-basic" defaultValue={editData?.end_location_name} onChange={(e) => { setSendData({ ...sendData, end_location_name: e?.target?.value }) }} label="end location" variant="outlined" color="common" />
               </Stack>
               <Stack style={{ marginTop: 20 }}>
-                <TextField disabled id="outlined-basic" defaultValue={editData?.klmtr} onChange={(e) => { setSendData({ ...sendData, klmtr: e?.target?.value }) }} label="end location" variant="outlined" color="common" />
+                <TextField disabled id="outlined-basic" defaultValue={editData?.klmtr} onChange={(e) => { setSendData({ ...sendData, klmtr: e?.target?.value }) }} label="Total Kilometer" variant="outlined" color="common" />
               </Stack>
               <br /><br />
             <div>
