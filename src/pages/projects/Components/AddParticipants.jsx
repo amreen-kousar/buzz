e8c4c0cd7a134f8fec318982ecba7bc63155ee47
aswin lohi <107166391+Aswinlohii@users.noreply.github.com>
2023-09-06 17:57:24 +0530

    validation in add and editparticipent

--- src/pages/projects/Components/AddParticipants.jsx
+++ src/pages/projects/Components/AddParticipants.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react';
-import {Button,CardContent,TextField,Select,MenuItem,InputLabel,Box} from '@mui/material';
+import {Button,CardContent,TextField,Select,MenuItem,InputLabel,Box, FormHelperText} from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import ListItemText from '@mui/material/ListItemText';
 import ListItem from '@mui/material/ListItem';
@@ -29,12 +29,14 @@ export default function AddParticipants({batch,checkData,type,session ,reloadFUn
         "participant_day1":"", 
         "nameOfSHG":"",
          "contact_no":"", "dob":"", "age":""}
-  
+         const [helperText, setHelperText] = React.useState('');
   const [open, setOpen] = React.useState(false);
    const [caste,setCaste] = useState([]);
    const [education,setEducation] = useState([]);
    const [participant, setParticipant] = useState(false);
    const [enterData,setEnterData] = React.useState(intialState)
+   const[educationError,setEducationError] = useState(false)
+   const[castError,setcastError] = useState(false)
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -89,7 +91,8 @@ export default function AddParticipants({batch,checkData,type,session ,reloadFUn
     if(enterData.age==""|| enterData.firstName==""|| enterData.caste =="" ||enterData.contact_no==""
     || enterData.husbandName==""|| enterData.nameOfSHG==""){
       alert("Please fil all the required data ")
-    }else{
+    }
+    else{
       handleClose()
       setOpen(false);
     }
@@ -124,9 +127,24 @@ export default function AddParticipants({batch,checkData,type,session ,reloadFUn
         participant_day1:(batch)?moment(day)?.format():'',
         type:(type=='vyapar' || type=="green")? type :moment(day)?.format()
       });
-      if(data.education =="" || data.husbandName == ""){
-alert("error!!!!!!")
-      }else{
+//       if(data.education =="" || data.husbandName == ""){
+// alert("error!!!!!!")
+//       }
+if(enterData.education ==""){
+  // alert('please Fill all the field')
+ setEducationError(true)
+ setHelperText('Please Select The Option');
+}
+if(enterData.caste ==""){
+  // alert('please Fill all the field')
+ setcastError(true)
+ setHelperText('Please Select The Option');
+}
+if(enterData.age==""|| enterData.firstName==""|| enterData.caste =="" ||enterData.contact_no==""
+|| enterData.husbandName==""|| enterData.nameOfSHG=="" || data.education =="" || data.husbandName == ""){
+  alert("Please fil all the required data ")
+}
+      else{
         var config = {
           method: 'post',
         maxBodyLength: Infinity,
@@ -143,6 +161,7 @@ alert("error!!!!!!")
           handleClose()
           handleCloseDilog()
           if(response?.data?.code ==200){
+            alert('Added Sucessfully')
             handleClose()
             handleCloseDilog()
               handleCloseSaveBtn()
@@ -213,6 +232,7 @@ alert("error!!!!!!")
               });
             }
   }
+
   return (
     <div>
       {(checkData?.data?.check_in_time_day1!="" )?
@@ -225,6 +245,10 @@ alert("error!!!!!!")
         onClose={handleClose}
         TransitionComponent={Transition}
       >
+        <form onSubmit={(e)=>{e.preventDefault(); handleApicall()}}>
+            {/* <form 
+           onSubmit={handleApicall}
+             >  */}
         <AppBar sx={{ position: 'fixed', bgcolor: '#ff7424' }}>
           <Toolbar>
             <IconButton
@@ -238,13 +262,19 @@ alert("error!!!!!!")
             <Typography sx={{ ml: 2, flex: 1, color:'white'}} variant="h6" component="div">
               Add Participants  
             </Typography>
-            <Button autoFocus color="inherit" onClick={handleApicall}>
+            <Button type='submit'
+           
+            //  onClick={handleApicall} 
+             autoFocus color="inherit">
               save
             </Button>
           </Toolbar>
         </AppBar>
         <Stack mt={10}>
         <CardContent>
+          
+
+          
         <TextField fullWidth id="Name"onChange={(e)=>{setEnterData({...enterData,firstName:e?.target?.value})}} label="Name" variant="outlined" required />
         </CardContent>
     </Stack>
@@ -255,28 +285,40 @@ alert("error!!!!!!")
     </Stack>
     <Stack mt={1}>
         <CardContent>
-        <TextField fullWidth id="fathername" onChange={(e)=>{setEnterData({...enterData,husbandName:e?.target?.value})}}label="Father/Husband name / Mother's name" variant="outlined" />
+        <TextField fullWidth id="fathername" required onChange={(e)=>{setEnterData({...enterData,husbandName:e?.target?.value})}}label="Father/Husband name / Mother's name" variant="outlined" />
         </CardContent>
     </Stack>
     <Stack mt={1}>
         <CardContent>
-        <TextField fullWidth id="Number" onChange={(e)=>{
+        <TextField fullWidth required id="Number" onChange={(e)=>{
           if(e.target.value.toString().length<= 10){
             setEnterData({...enterData,contact_no:e?.target?.value})
           }
           }} type="Number" label="Contact Number" variant="outlined" 
           value={enterData.contact_no}
+          inputProps={{  inputMode: 'numeric',
+
+          pattern: '[0-9]*', maxLength: 10 }}
           
           />
         </CardContent>
     </Stack>
     <Stack mt={1}>
         <CardContent>
-        <TextField fullWidth id="shg" onChange={(e)=>{setEnterData({...enterData,nameOfSHG:e?.target?.value})}} label="Name of SHG" variant="outlined" />
+        <TextField required fullWidth id="shg" onChange={(e)=>{setEnterData({...enterData,nameOfSHG:e?.target?.value})}} label="Name of SHG" variant="outlined" />
         </CardContent>
     </Stack>
     <CardContent>
-    <InputLabel id="demo-simple-select-label">Caste</InputLabel>
+    <InputLabel id="demo-simple-select-label">
+     
+      <Typography>
+      
+      Caste
+      {castError ? (
+                            <FormHelperText style={{ color: 'red' }}>{helperText}</FormHelperText>
+                          ) : null}{' '}
+      </Typography>
+      </InputLabel>
   <Select
   fullWidth
     labelId="demo-simple-select-label"
@@ -294,7 +336,15 @@ alert("error!!!!!!")
   </Select>
   </CardContent>
   <CardContent>
-  <InputLabel id="demo-simple-select-label">Education</InputLabel>
+  <InputLabel id="demo-simple-select-label">
+    <Typography>
+      
+    Education
+    {educationError ? (
+                          <FormHelperText style={{ color: 'red' }}>{helperText}</FormHelperText>
+                        ) : null}{' '}
+    </Typography>
+  </InputLabel>
   <Select
     fullWidth
     labelId="demo-simple-select-label"
@@ -312,6 +362,7 @@ alert("error!!!!!!")
     })}
   </Select>
   </CardContent>
+  </form>
       </Dialog>
     </div>
   );
--- src/pages/projects/Components/Editparticipantdata.jsx
+++ src/pages/projects/Components/Editparticipantdata.jsx
@@ -1,6 +1,18 @@
 import { useState, useEffect } from 'react';
-import React from 'react'
-import { Button, Card, CardActions, CardContent, Stack,TextField ,Radio, DialogContent, DialogContentText,FormControlLabel} from '@mui/material';
+import React from 'react';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Stack,
+  TextField,
+  Radio,
+  DialogContent,
+  DialogContentText,
+  FormControlLabel,
+  FormHelperText,
+} from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
@@ -29,188 +41,321 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
-export default function EditParticipantdata({editSession, setEditsession,Trainingdata, changeState,participantdata,cvalue}) {
+export default function EditParticipantdata({
+  editSession,
+  setEditsession,
+  Trainingdata,
+  changeState,
+  participantdata,
+  cvalue,
+}) {
   const [openFilter, setOpenFilter] = useState(false);
- const [showDate , setShowDate] = useState(false)
+  const [showDate, setShowDate] = useState(false);
   const [sendData, setSendData] = useState({
-    occupation:"",
-    husbandOccupation:"",
-    wifeIncomeMonthly:"",
-    typeOfEnterprise:"",
-    saving_amt:"",
+    occupation: '',
+    husbandOccupation: '',
+    wifeIncomeMonthly: '',
+    typeOfEnterprise: '',
+    saving_amt: '',
     gelathiRecomm: 0,
-    saving_goal:"",
-    wifeSavingsMonthly:"",
-    income:"",
-    bank_acc:""
+    saving_goal: '',
+    wifeSavingsMonthly: '',
+    income: '',
+    bank_acc: '',
   });
+  const [helperText, setHelperText] = useState('');
   const [open, setOpen] = React.useState(false);
   const [date, setDate] = useState(new Date());
-  const [occupationdata,setOccupationdata]=React.useState('');
+  const [occupationdata, setOccupationdata] = React.useState('');
+  const [HusbandOccupationError, setHusbandOccupationError] = useState(false);
+  const [OccupationError, setOccupationError] = useState(false);
+  const [BankError, setBankError] = useState(false);
+
   React.useEffect(() => {
     //setShown(shown)
-    setOpen(editSession)
-    
-  }, [editSession])
-  React.useEffect(()=>{
-    Occupation()
-  },[])
+    setOpen(editSession);
+  }, [editSession]);
+  React.useEffect(() => {
+    Occupation();
+  }, []);
   const handleClickOpen = () => {
-    setEditsession(true)
+    setEditsession(true);
     setOpen(true);
   };
   const handleClose = () => {
-    setEditsession(false)
+    setEditsession(false);
     setOpen(false);
     changeState();
   };
-const Occupation =()=>{
+  const Occupation = () => {
     var config = {
-        method: 'get',
-        url: 'https://bdms.buzzwomen.org/appTest/getOccupations.php',
-        headers: { }
-      };
-      
-      axios(config)
-      .then(function (response) {
-        setOccupationdata(response.data)
-      })
-      .catch(function (error) {
-        // console.log(error);
-      });
-      
-}
-const SendData = async => {
-    var data = JSON.stringify({
-        "income":sendData?.income, 
-        "occupation":sendData?.occupation,
-         "typeOfEnterprise":sendData?.typeOfEnterprise, 
-         "participant_id":participantdata?.id, 
-         "final_save":1, 
-         "husbandOccupation":sendData?.husbandOccupation,
-         "wifeIncomeMonthly":sendData?.wifeIncomeMonthly, 
-         "saving_goal":sendData?.saving_goal, 
-         "bank_acc":sendData?.bank_acc, 
-         "wifeSavingsMonthly":sendData?.wifeSavingsMonthly,
-         "saving_amt":sendData?.saving_amt, 
-         "participant_day2":Trainingdata?.data?.day2, 
-         "gelathiRecomm":sendData?.gelathiRecomm, 
-         "project_id":Trainingdata?.data?.project_id, 
-         "tb_id":Trainingdata?.data?.id
-    });
-    var config = {
-      method: 'post',
-      url: 'https://bdms.buzzwomen.org/appTest/editParticipant.php',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      data: data
+      method: 'get',
+      url: 'https://bdms.buzzwomen.org/appTest/getOccupations.php',
+      headers: {},
     };
+
     axios(config)
       .then(function (response) {
-        setSendData({
-          occupation:"",
-          husbandOccupation:"",
-          wifeIncomeMonthly:"",
-          typeOfEnterprise:"",
-          saving_amt:"",
-          gelathiRecomm: 0,
-          saving_goal:"",
-          wifeSavingsMonthly:"",
-          income:"",
-          bank_acc:""
-      
-        })
-        handleClose()
-        changeState();
+        setOccupationdata(response.data);
       })
       .catch(function (error) {
         // console.log(error);
       });
-  }
+  };
+  const SendData = (async) => {
+    var data = JSON.stringify({
+      income: sendData?.income,
+      occupation: sendData?.occupation,
+      typeOfEnterprise: sendData?.typeOfEnterprise,
+      participant_id: participantdata?.id,
+      final_save: 1,
+      husbandOccupation: sendData?.husbandOccupation,
+      wifeIncomeMonthly: sendData?.wifeIncomeMonthly,
+      saving_goal: sendData?.saving_goal,
+      bank_acc: sendData?.bank_acc,
+      wifeSavingsMonthly: sendData?.wifeSavingsMonthly,
+      saving_amt: sendData?.saving_amt,
+      participant_day2: Trainingdata?.data?.day2,
+      gelathiRecomm: sendData?.gelathiRecomm,
+      project_id: Trainingdata?.data?.project_id,
+      tb_id: Trainingdata?.data?.id,
+    });
+    if (sendData?.husbandOccupation == '') {
+      setHusbandOccupationError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (sendData?.occupation == '') {
+      setOccupationError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (sendData?.bank_acc == '') {
+      setBankError(true);
+      setHelperText('Please Select The Option');
+    }
+    if (sendData?.husbandOccupation == '' || sendData?.occupation == '') {
+      alert("Please Select The Option'");
+    } else {
+      var config = {
+        method: 'post',
+        url: 'https://bdms.buzzwomen.org/appTest/editParticipant.php',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: data,
+      };
+      axios(config)
+        .then(function (response) {
+          setSendData({
+            occupation: '',
+            husbandOccupation: '',
+            wifeIncomeMonthly: '',
+            typeOfEnterprise: '',
+            saving_amt: '',
+            gelathiRecomm: 0,
+            saving_goal: '',
+            wifeSavingsMonthly: '',
+            income: '',
+            bank_acc: '',
+          });
+          handleClose();
+          changeState();
+        })
+        .catch(function (error) {
+          // console.log(error);
+        });
+    }
+  };
   return (
     <div>
-        <Dialog fullScreen open={open} onClose={handleClose}
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
         aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description">
-        <Toolbar sx={{ bgcolor: '#ed6c02', color: 'white' }} >
-          <IconButton edge="start" sx={{ color: "inherit" }} onClick={handleClose} aria-label="close">
-            <CloseIcon />
-          </IconButton>
-          <Typography sx={{ ml: 2, flex: 1, color: "inherit" }} variant="h6" component="div" >
-           Update Participant Detail 
-          </Typography>
-        
-          <Button autoFocus color="inherit" onClick={() => SendData()}>
-            save
-          </Button>
-        </Toolbar>
-      
-        <DialogContent dividers={scroll === 'paper'} sx={{ background: "#f9fafb" }}>
-          <DialogContentText
-            id="scroll-dialog-description"
-            tabIndex={-1}
-          >
-            <FormControl fullWidth >
-                 <InputLabel id="demo-simple-select-label" style={{ flexDirection: 'row', color: '#ed6c02', fontWeight: 700 ,marginTop:20}}>
-                    {sendData?.occupation== "" ? "Select wife's occupation" : "Wife occupation"}</InputLabel>  <br/><br/>
-                    <Select labelId="Select Wife's Occupation" id="demo-simple-select" value={sendData?.occupation} label="Wife Occupation" onChange={(e) => setSendData({ ...sendData, occupation: e?.target?.value })} variant="standard" color="common">
-                    {occupationdata?.list?.map(itm => {
-                      return (<MenuItem value={itm?.id}>{itm?.name}</MenuItem>)
-                    })}
-                  
-                  </Select>
-                  </FormControl> 
-                  <FormControl fullWidth >
-                 <InputLabel id="demo-simple-select-label" style={{ flexDirection: 'row', color: '#ed6c02', fontWeight: 700 ,marginTop:30}}>
-                    {sendData?.husbandOccupation== "" ? "Select Husband's occupation" : "Husband occupation"}</InputLabel>  <br/><br/><br/>
-                    <Select labelId="Select Wife's Occupation" id="demo-simple-select" value={sendData?.husbandOccupation} label="Husband Occupation" onChange={(e) => setSendData({ ...sendData, husbandOccupation: e?.target?.value })} variant="standard" color="common">
-                    {occupationdata?.list?.map(itm => {
-                      return (<MenuItem value={itm?.id}>{itm?.name}</MenuItem>)
-                    })}
-                   
-                  </Select>
-                  </FormControl> 
-                  <Stack style={{ marginTop: 20 }}>
-                <TextField id="Monthly-income" onChange={(e) => { setSendData({ ...sendData,wifeIncomeMonthly : e?.target?.value }) }} label="Monthly Wife's income" variant="outlined" color="common"/>
+        aria-describedby="scroll-dialog-description"
+      >
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            SendData();
+          }}
+        >
+          <Toolbar sx={{ bgcolor: '#ed6c02', color: 'white' }}>
+            <IconButton edge="start" sx={{ color: 'inherit' }} onClick={handleClose} aria-label="close">
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1, color: 'inherit' }} variant="h6" component="div">
+              Update Participant Detail
+            </Typography>
+
+            <Button
+              autoFocus
+              color="inherit"
+              //  onClick={() => SendData()}
+              type="submit"
+            >
+              save
+            </Button>
+          </Toolbar>
+
+          <DialogContent dividers={scroll === 'paper'} sx={{ background: '#f9fafb' }}>
+            <DialogContentText id="scroll-dialog-description" tabIndex={-1}>
+              <FormControl fullWidth>
+                {OccupationError ? <FormHelperText style={{ color: 'red' }}>{helperText}</FormHelperText> : null}{' '}
+                <InputLabel
+                  id="demo-simple-select-label"
+                  style={{ flexDirection: 'row', color: '#ed6c02', fontWeight: 700, marginTop: 20 }}
+                >
+                  {sendData?.occupation == '' ? "Select wife's occupation" : 'Wife occupation'}
+                </InputLabel>{' '}
+                <br />
+                <br />
+                <Select
+                  labelId="Select Wife's Occupation"
+                  id="demo-simple-select"
+                  value={sendData?.occupation}
+                  label="Wife Occupation"
+                  onChange={(e) => setSendData({ ...sendData, occupation: e?.target?.value })}
+                  variant="standard"
+                  color="common"
+                >
+                  {occupationdata?.list?.map((itm) => {
+                    return <MenuItem value={itm?.id}>{itm?.name}</MenuItem>;
+                  })}
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                {HusbandOccupationError ? <FormHelperText style={{ color: 'red' }}>{helperText}</FormHelperText> : null}{' '}
+                <InputLabel
+                  id="demo-simple-select-label"
+                  style={{ flexDirection: 'row', color: '#ed6c02', fontWeight: 700, marginTop: 30 }}
+                >
+                  {sendData?.husbandOccupation == '' ? "Select Husband's occupation" : 'Husband occupation'}
+                </InputLabel>{' '}
+                <br />
+                <br />
+                <br />
+                <Select
+                  labelId="Select Wife's Occupation"
+                  id="demo-simple-select"
+                  value={sendData?.husbandOccupation}
+                  label="Husband Occupation"
+                  onChange={(e) => setSendData({ ...sendData, husbandOccupation: e?.target?.value })}
+                  variant="standard"
+                  color="common"
+                >
+                  {occupationdata?.list?.map((itm) => {
+                    return <MenuItem value={itm?.id}>{itm?.name}</MenuItem>;
+                  })}
+                </Select>
+              </FormControl>
+              <Stack style={{ marginTop: 20 }}>
+                <TextField
+                  required
+                  id="Monthly-income"
+                  onChange={(e) => {
+                    setSendData({ ...sendData, wifeIncomeMonthly: e?.target?.value });
+                  }}
+                  label="Monthly Wife's income"
+                  variant="outlined"
+                  color="common"
+                />
               </Stack>
               <Stack style={{ marginTop: 20 }}>
-                <TextField id="Monthly-savings" onChange={(e) => { setSendData({ ...sendData,wifeSavingsMonthly : e?.target?.value }) }} label="Monthly Wife's Savings" variant="outlined" color="common"/>
+                <TextField
+                  required
+                  id="Monthly-savings"
+                  onChange={(e) => {
+                    setSendData({ ...sendData, wifeSavingsMonthly: e?.target?.value });
+                  }}
+                  label="Monthly Wife's Savings"
+                  variant="outlined"
+                  color="common"
+                />
               </Stack>
               <Stack style={{ marginTop: 20 }}>
-                <TextField id="goal" onChange={(e) => { setSendData({ ...sendData,saving_goal : e?.target?.value }) }} label="What is your goal" variant="outlined" color="common"/>
+                <TextField
+                  required
+                  id="goal"
+                  onChange={(e) => {
+                    setSendData({ ...sendData, saving_goal: e?.target?.value });
+                  }}
+                  label="What is your goal"
+                  variant="outlined"
+                  color="common"
+                />
               </Stack>
               <Stack style={{ marginTop: 20 }}>
-                <TextField id="Monthly family income" onChange={(e) => { setSendData({ ...sendData,income: e?.target?.value }) }} label="Monthly Family Income" variant="outlined" color="common"/>
+                <TextField
+                  required
+                  id="Monthly family income"
+                  onChange={(e) => {
+                    setSendData({ ...sendData, income: e?.target?.value });
+                  }}
+                  label="Monthly Family Income"
+                  variant="outlined"
+                  color="common"
+                />
               </Stack>
               <Stack style={{ marginTop: 20 }}>
-                <TextField id="Monthly _family_savings" onChange={(e) => { setSendData({ ...sendData,saving_amt : e?.target?.value }) }} label="Monthly Family Savings" variant="outlined" color="common"/>
+                <TextField
+                  required
+                  id="Monthly _family_savings"
+                  onChange={(e) => {
+                    setSendData({ ...sendData, saving_amt: e?.target?.value });
+                  }}
+                  label="Monthly Family Savings"
+                  variant="outlined"
+                  color="common"
+                />
               </Stack>
               <Stack style={{ marginTop: 20 }}>
-                <TextField id="type_of_enterprise" onChange={(e) => { setSendData({ ...sendData,typeOfEnterprise : e?.target?.value }) }} label="Type of Enterprise" variant="outlined" color="common"/>
+                <TextField
+                  required
+                  id="type_of_enterprise"
+                  onChange={(e) => {
+                    setSendData({ ...sendData, typeOfEnterprise: e?.target?.value });
+                  }}
+                  label="Type of Enterprise"
+                  variant="outlined"
+                  color="common"
+                />
               </Stack>
               <Stack mt={1}>
-                <Typography style={{fontWeight:500}} >Bank Account *</Typography>
+                <Typography style={{ fontWeight: 500 }}>Bank Account *</Typography>
+                <Typography>
+                  {' '}
+                  {BankError ? <FormHelperText style={{ color: 'red' }}>{helperText}</FormHelperText> : null}{' '}
+                </Typography>
                 <RadioGroup
-                      aria-labelledby="demo-radio-buttons-group-label"
-                      name="radio-buttons-group"
-                      onChange={(e, value) => { setSendData({ ...sendData, bank_acc: value }) }}
-                  
-                    >
-                    <div style={{display:"flex"}}>
-                      <FormControlLabel value={0} control={<Radio style={{color:"#595959"}} />} label="No" />
-                      <FormControlLabel value={1} control={<Radio style={{color:"#595959"}}  />} label="Yes" />
-                      </div>
-                    </RadioGroup>
-            </Stack>
-            <Stack id="create-poa-stack" direction={'row'}>
-                  <Typography id="all-day">Suggested Gelathi</Typography><br/>
-                  <Switch id="switch-suggested-gelathi" value={sendData?.gelathiRecomm} 
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  name="radio-buttons-group"
+                  onChange={(e, value) => {
+                    setSendData({ ...sendData, bank_acc: value });
+                  }}
+                  required
+                >
+                  <div style={{ display: 'flex' }}>
+                    <FormControlLabel value={0} control={<Radio style={{ color: '#595959' }} />} label="No" />
+                    <FormControlLabel value={1} control={<Radio style={{ color: '#595959' }} />} label="Yes" />
+                  </div>
+                </RadioGroup>
+              </Stack>
+              <Stack id="create-poa-stack" direction={'row'}>
+                <Typography id="all-day">Suggested Gelathi</Typography>
+                <br />
+                <Switch
+                  id="switch-suggested-gelathi"
+                  value={sendData?.gelathiRecomm}
                   onChange={(e) => {
-                     setSendData({ ...sendData, gelathiRecomm: sendData?.gelathiRecomm === 1 ? 0 : 1 }) 
-                  }}/>
-                </Stack>
-          </DialogContentText></DialogContent>  </Dialog>
+                    setSendData({ ...sendData, gelathiRecomm: sendData?.gelathiRecomm === 1 ? 0 : 1 });
+                  }}
+                />
+              </Stack>
+            </DialogContentText>
+          </DialogContent>
+        </form>
+      </Dialog>
     </div>
   );
 }
